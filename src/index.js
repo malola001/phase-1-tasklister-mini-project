451b803d71ecbc46de798d34656268f724652bec
malola001 <hopejulie36@gmail.com>
2024-10-06 20:18:06 +0300

    add complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      createTodo(e.target["new-task-description"].value);
+      form.reset();
+  });
 });
+
+function createTodo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "Delete";
+  li.textContent = todo; 
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
